f74bff76148b0e45f0a00f36a6e91c7c5ac55c5f
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2019-12-01 19:09:43 +0100

    mvvmfx bindings

--- src/main/java/sample/SampleView.java
+++ src/main/java/sample/SampleView.java
@@ -1,13 +1,33 @@
 package sample;
 
 import de.saxsys.mvvmfx.FxmlView;
+import de.saxsys.mvvmfx.InjectViewModel;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SampleView implements FxmlView<SampleViewModel>, Initializable {
+    @FXML
+    public Label halloLabel;
+
+    @FXML
+    public Button buhButton;
+
+    @InjectViewModel
+    private SampleViewModel viewModel;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        halloLabel.textProperty().bind(viewModel.halloLabelTextProperty());
+        halloLabel.disableProperty().bind(viewModel.getBuhCommand().notExecutableProperty());
+    }
+
+    @FXML
+    public void buhButtonAction() {
+        viewModel.getBuhCommand().execute();
     }
 }
--- src/main/java/sample/SampleViewModel.java
+++ src/main/java/sample/SampleViewModel.java
@@ -1,7 +1,31 @@
 package sample;
 
 import de.saxsys.mvvmfx.ViewModel;
+import de.saxsys.mvvmfx.utils.commands.Action;
+import de.saxsys.mvvmfx.utils.commands.Command;
+import de.saxsys.mvvmfx.utils.commands.DelegateCommand;
+import javafx.beans.property.ReadOnlyStringProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 public class SampleViewModel implements ViewModel {
+    private final StringProperty halloLabelText = new SimpleStringProperty("hallo1");
+    private final Command buhCommand;
 
+    public SampleViewModel() {
+        buhCommand = new DelegateCommand(() -> new Action() {
+            @Override
+            protected void action() {
+                halloLabelText.set("hallo buh");
+            }
+        });
+    }
+
+    public ReadOnlyStringProperty halloLabelTextProperty() {
+        return halloLabelText;
+    }
+
+    public Command getBuhCommand() {
+        return buhCommand;
+    }
 }
