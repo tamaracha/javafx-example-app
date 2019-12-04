2f1370da2d2a448ac7a67e2d5077920ed01824d6
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2019-12-04 17:20:33 +0100

    Create ListView example

--- src/main/kotlin/sample/SampleView.kt
+++ src/main/kotlin/sample/SampleView.kt
@@ -5,10 +5,18 @@ import de.saxsys.mvvmfx.InjectViewModel
 import javafx.fxml.FXML
 import javafx.scene.control.Button
 import javafx.scene.control.Label
+import javafx.scene.control.ListView
+import javafx.scene.control.TextArea
 
 public class SampleView : FxmlView<SampleViewModel> {
     @FXML
-    public lateinit var halloLabel: Label
+    public lateinit var schnippLabel: Label
+
+    @FXML
+    public lateinit var schnippSelect: ListView<String>
+
+    @FXML
+    public lateinit var newBoohooField: TextArea
 
     @FXML
     public lateinit var buhButton: Button
@@ -17,12 +25,26 @@ public class SampleView : FxmlView<SampleViewModel> {
     private lateinit var viewModel: SampleViewModel
 
     public fun initialize() {
-        halloLabel.textProperty().bind(viewModel.halloLabelText)
-        buhButton.disableProperty().bind(viewModel.buhCommand.notExecutableProperty())
+        schnippLabel.textProperty().bind(viewModel.selectedBoohoo)
+        newBoohooField.textProperty().bindBidirectional(viewModel.newBoohooText)
+        schnippSelect.setPlaceholder(Label("alles"))
+        schnippSelect.itemsProperty().bind(viewModel.boohooList)
+        schnippSelect.getSelectionModel().selectedItemProperty().addListener({ obs, oldValue, newValue ->
+            if (oldValue != newValue && newValue != viewModel.selectedBoohoo.getValue()) {
+                viewModel.selectedBoohoo.set(newValue)
+            }
+        })
+        viewModel.selectedBoohoo.addListener({ obs, oldValue, newValue ->
+            if (oldValue != newValue && newValue != schnippSelect.getSelectionModel().getSelectedItem()) {
+                schnippSelect.getSelectionModel().select(newValue)
+            }
+        })
+        // viewModel.selectedBoohoo.bind(schnippSelect.getSelectionModel().selectedItemProperty())
+        buhButton.disableProperty().bind(viewModel.addBoohooCommand.notExecutableProperty())
     }
 
     @FXML
     public fun buhButtonAction() {
-        viewModel.buhCommand.execute()
+        viewModel.addBoohooCommand.execute()
     }
 }
--- src/main/kotlin/sample/SampleViewModel.kt
+++ src/main/kotlin/sample/SampleViewModel.kt
@@ -4,16 +4,24 @@ import de.saxsys.mvvmfx.ViewModel
 import de.saxsys.mvvmfx.utils.commands.Action
 import de.saxsys.mvvmfx.utils.commands.Command
 import de.saxsys.mvvmfx.utils.commands.DelegateCommand
+import javafx.beans.property.SimpleListProperty
 import javafx.beans.property.SimpleStringProperty
 import javafx.beans.property.StringProperty
+import javafx.collections.FXCollections
 
 public class SampleViewModel : ViewModel {
-    private val halloLabelInitialText = "hallo1"
+    private val initialBoohooText = "Buhu"
+    private val initialItems = listOf("Schnipp", "Schnapp", "Schnupp")
 
-    public val halloLabelText: StringProperty = SimpleStringProperty(halloLabelInitialText)
-    public val buhCommand: Command = DelegateCommand({ object : Action() {
+    public val selectedBoohoo: StringProperty = SimpleStringProperty()
+    public val newBoohooText: StringProperty = SimpleStringProperty(initialBoohooText)
+    public val boohooList = SimpleListProperty(FXCollections.observableArrayList(initialItems))
+    public val addBoohooCommand: Command = DelegateCommand({ object : Action() {
         override fun action() {
-            halloLabelText.set("hallo buh")
+            val value: String = newBoohooText.getValue()
+            boohooList.add(value)
+            newBoohooText.set("")
+            selectedBoohoo.set(value)
         }
-    } }, halloLabelText.isEqualTo(halloLabelInitialText))
+    } }, newBoohooText.isNotEmpty())
 }
